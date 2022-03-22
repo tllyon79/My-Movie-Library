f163a80cc45651b3324456dc57c24f19072fc2cc
Silvris <tllyon79@gmail.com>
2022-03-22 10:15:26 -0500

    Quick push to set up Git within IntelliJ

--- src/mml/Base/Main.java
+++ src/mml/Base/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
