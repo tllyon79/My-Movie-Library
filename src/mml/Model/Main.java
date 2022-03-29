6abc1b01185cdb65ab645c14bcc7a4cd92d954a5
Silvris <100308223+tllyon79@users.noreply.github.com>
2022-03-29 10:00:49 -0500

    push test two, proper account this time?

--- src/mml/Model/Main.java
+++ src/mml/Model/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World");
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
