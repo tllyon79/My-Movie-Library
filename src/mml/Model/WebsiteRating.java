aaa32917787260bae70b4945de243f52360dd3be
Silvris <100308223+tllyon79@users.noreply.github.com>
2022-04-16 17:19:41 -0500

    fix sort by rating

--- src/mml/Model/WebsiteRating.java
+++ src/mml/Model/WebsiteRating.java
@@ -9,6 +9,19 @@ public class WebsiteRating {
     }
 
     public Double GetValueAsDouble(){
-        return Double.parseDouble(Value);
+        String[] vals = new String[2];
+        if (Value.contains("%")){
+            vals[0] = Value.replace("%","");
+            vals[1] = "100";
+        }
+        else if (Value.contains("/")){
+            vals = Value.split("/");
+        }
+        else{
+            vals = Value.split("\\\\");
+        }
+        if(vals.length != 2) return 0d;
+
+        return Double.parseDouble(vals[0]) / Double.parseDouble(vals[1]);
     }
 }
